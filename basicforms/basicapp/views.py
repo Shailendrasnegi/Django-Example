087f13bda03765bd53308c30b5f071aa489d33f2
Shailendra Singh <shailendrasnegi@gmail.com>
2021-02-13 16:32:57 +0530

    first

---
+++ basicforms/basicapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from . import forms
+
+# Create your views here.
+def index(request):
+    return render(request, 'basicapp/index.html')
+
+def form_name_view(request):
+    form = forms.FormName()
+    if request.method =='POST':
+        form=forms.FormName(request.POST)
+        if form.is_valid():
+            print("VALIDATION SUCCESS")
+            print("NAME: "+form.cleaned_data['name'])
+            print("EMAIL: "+form.cleaned_data['name'])
+            print("TEXT: "+form.cleaned_data['name'])
+    return render(request, 'basicapp/form_page.html', {'form':form})
